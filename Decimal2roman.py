4d61b64e413c398a903a719c80366f3ba875d9c4
RafaRuizz <ri.ruiz@alumno.um.edu.ar>
2023-04-11 09:18:18 -0300

    initial commit

---
+++ Decimal2roman.py
@@ -0,0 +1,17 @@
+import unittest
+
+def decimal2roman(decimal)
+    numeros = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    romanos = ["M", "MC", "D", "CD","C","XC","L","XL","X","IX","V","IV","I"]
+
+    aux=""
+    i = 0
+    while decimal > 0:
+        for j in range(decimal // numeros[i]):
+            aux += romanos [i]
+            decimal -= numeros [i]
+        i += 1
+    return aux
+
+if __name__=="__main__":
+    unittest.main()    
